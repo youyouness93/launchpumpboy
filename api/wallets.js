2b2756c59d0ad946a8b68b826bb84d42adb99a14
youyouness93 <younesslasry@gmail.com>
2025-01-09 18:03:57 -0500

    Fix CORS and update API domain

--- api/wallets.js
+++ api/wallets.js
@@ -3,7 +3,7 @@ import prisma from '../lib/prisma'
 export default async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', 'https://pump-boy.fun')
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
 
